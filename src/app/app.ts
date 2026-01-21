1c223b27fc7a8e98a3667716a675f981a7f01f6c
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-21 15:08:12 +0100

    background img added as files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,33 @@
+import { HeroContent } from './sections/hero-content/hero-content';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './sections/hero/hero';
+import { HeroHeader } from './sections/hero-header/hero-header';
+import { Logo } from './sections/hero-header/logo/logo';
+import { Navigation } from './sections/hero-header/navigation/navigation';
+import { LanguageSwitch } from './sections/hero-header/language-switch/language-switch';
+import { Badge } from './sections/hero-content/badge/badge';
+import { Cta } from './sections/hero-content/cta/cta';
+import { Title } from './sections/hero-content/title/title';
+import { HeroProfile } from './sections/hero-profile/hero-profile';
+import { HeroSocials } from './sections/hero-socials/hero-socials';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero],
+  imports: [
+    RouterOutlet,
+    Hero,
+    HeroHeader,
+    Logo,
+    Navigation,
+    LanguageSwitch,
+    Badge,
+    HeroContent,
+    Cta,
+    Title,
+    HeroProfile,
+    HeroSocials,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
