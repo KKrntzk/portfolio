803cb9178841d9ce754e197a8b977354f2584889
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-22 07:49:50 +0100

    nav parts added into it

--- src/app/sections/hero-header/navigation/navigation.ts
+++ src/app/sections/hero-header/navigation/navigation.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Skills } from './skills/skills';
+import { AboutMe } from './about-me/about-me';
+import { Contact } from './contact/contact';
+import { Projects } from './projects/projects';
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [Skills, AboutMe, Contact, Projects],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss',
 })
-export class Navigation {
-
-}
+export class Navigation {}
