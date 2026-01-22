7b4da7c9b33cb39a2a6819a2ca40ce1507517f1c
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-22 09:51:06 +0100

    header styling

--- src/app/sections/hero-header/hero-header.ts
+++ src/app/sections/hero-header/hero-header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Navigation } from './navigation/navigation';
+import { Logo } from './logo/logo';
+import { LanguageSwitch } from './language-switch/language-switch';
 
 @Component({
   selector: 'app-hero-header',
-  imports: [],
+  imports: [Navigation, Logo, LanguageSwitch],
   templateUrl: './hero-header.html',
   styleUrl: './hero-header.scss',
 })
-export class HeroHeader {
-
-}
+export class HeroHeader {}
