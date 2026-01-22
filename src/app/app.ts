cd7e47726909e5dd227bd926fa0cc850c513a1e7
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-22 07:45:18 +0100

    added more folders to nav

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,6 +11,10 @@ import { Cta } from './sections/hero-content/cta/cta';
 import { Title } from './sections/hero-content/title/title';
 import { HeroProfile } from './sections/hero-profile/hero-profile';
 import { HeroSocials } from './sections/hero-socials/hero-socials';
+import { AboutMe } from './sections/hero-header/navigation/about-me/about-me';
+import { Contact } from './sections/hero-header/navigation/contact/contact';
+import { Skills } from './sections/hero-header/navigation/skills/skills';
+import { Projects } from './sections/hero-header/navigation/projects/projects';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +31,10 @@ import { HeroSocials } from './sections/hero-socials/hero-socials';
     Title,
     HeroProfile,
     HeroSocials,
+    AboutMe,
+    Contact,
+    Skills,
+    Projects,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
