b665e639594d1da745a546712507f434bf3d17ef
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-22 10:25:02 +0100

    font added, header postioning fixed

--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroHeader } from '../hero-header/hero-header';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [HeroHeader],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {
-
-}
+export class Hero {}
