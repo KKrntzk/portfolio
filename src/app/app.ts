b713064a21e9f0d8c446faff52b2c85ed55b1a43
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-26 08:26:09 +0100

    startet on skill title

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Hero } from './sections/hero/hero';
 import { HeroHeader } from './sections/hero-header/hero-header';
 import { AboutMeSection } from './sections/about-me-section/about-me-section';
+import { SkillSet } from './sections/skill-set/skill-set';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, HeroHeader, AboutMeSection],
+  imports: [RouterOutlet, Hero, AboutMeSection, SkillSet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/sections/skill-set/skill-set.ts
+++ src/app/sections/skill-set/skill-set.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { PullBadge } from './pull-badge/pull-badge';
+import { SkillTitle } from './skill-title/skill-title';
+import { StackDescribtion } from './stack-describtion/stack-describtion';
+import { StackIcons } from './stack-icons/stack-icons';
 
 @Component({
   selector: 'app-skill-set',
-  imports: [],
+  imports: [PullBadge, SkillTitle, StackDescribtion, StackIcons],
   templateUrl: './skill-set.html',
   styleUrl: './skill-set.scss',
 })
-export class SkillSet {
-
-}
+export class SkillSet {}
