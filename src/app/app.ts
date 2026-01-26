a4ce22f3b72de99ad9598591116420caf673b184
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-26 14:56:24 +0100

    added new files, started with project-examples

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Hero } from './sections/hero/hero';
 import { HeroHeader } from './sections/hero-header/hero-header';
 import { AboutMeSection } from './sections/about-me-section/about-me-section';
 import { SkillSet } from './sections/skill-set/skill-set';
+import { ProjectsComponent } from './sections/projects-component/projects-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, AboutMeSection, SkillSet],
+  imports: [RouterOutlet, Hero, AboutMeSection, SkillSet, ProjectsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/sections/projects-component/projects-component.ts
+++ src/app/sections/projects-component/projects-component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { ProjectsMain } from './projects-main/projects-main';
+import { ProjectExamples } from './project-examples/project-examples';
 
 @Component({
   selector: 'app-projects-component',
-  imports: [],
+  imports: [ProjectsMain, ProjectExamples],
   templateUrl: './projects-component.html',
   styleUrl: './projects-component.scss',
 })
-export class ProjectsComponent {
-
-}
+export class ProjectsComponent {}
