df265b6c6fba66b020dad491166599deb0e7ffb8
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-27 10:10:08 +0100

    added folders for next section

---
+++ src/app/sections/contact-component/contact-main/contact-main.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-main',
+  imports: [],
+  templateUrl: './contact-main.html',
+  styleUrl: './contact-main.scss',
+})
+export class ContactMain {
+
+}
