1bf9945e1b6d8a03ab01fc4f3aafbdacd84c4b05
KKrntzk <sophie.kornetzki@gmail.com>
2026-01-29 17:17:06 +0100

    github links added

--- src/app/sections/contact-component/footer/footer.ts
+++ src/app/sections/contact-component/footer/footer.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './footer.scss',
 })
 export class Footer {
-
+  openGithub() {
+    window.open('https://github.com/KKrntzk', '_blank');
+  }
 }
--- src/app/sections/hero/socials/socials.ts
+++ src/app/sections/hero/socials/socials.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-socials',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './socials.html',
   styleUrl: './socials.scss',
 })
-export class Socials {
-
-}
+export class Socials {}
