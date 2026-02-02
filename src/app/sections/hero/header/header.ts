d94625a8bda168f4c5a3ae5d8bdd5fd5d84d1520
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-02 15:16:29 +0100

    drop down updated

--- src/app/sections/hero/header/header.ts
+++ src/app/sections/hero/header/header.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { PersonalLogo } from './personal-logo/personal-logo';
 import { Navbar } from './navbar/navbar';
 import { LgSwitch } from './lg-switch/lg-switch';
+import { Socials } from '../socials/socials';
 
 @Component({
   selector: 'app-header',
-  imports: [PersonalLogo, Navbar, LgSwitch],
+  imports: [PersonalLogo, Navbar, LgSwitch, Socials],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
