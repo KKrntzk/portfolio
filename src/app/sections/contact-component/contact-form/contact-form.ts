7386e496fbdbd25fb34b15ca349e176b0e77bdb3
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-02 11:45:44 +0100

    forms logic added

--- src/app/sections/contact-component/contact-form/contact-form.ts
+++ src/app/sections/contact-component/contact-form/contact-form.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function emailDomainValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value as string;
+    if (!value) {
+      return null;
+    }
+    const isValid = value.toLowerCase().endsWith('.de') || value.toLowerCase().endsWith('.com');
+    return isValid ? null : { invalidDomain: true };
+  };
+}
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
 export class ContactForm {
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email, emailDomainValidator()]),
+    message: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    privacy: new FormControl(false, [Validators.requiredTrue]),
+  });
 
+  onSubmit() {
+    console.log('Formular erfolgreich gesendet:', this.contactForm.value);
+  }
 }
