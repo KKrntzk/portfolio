c126a975c87a2e52616f72c3cc7061dc00f4617e
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-03 12:45:39 +0100

    logo added to last section

--- src/app/sections/contact-component/contact-component.ts
+++ src/app/sections/contact-component/contact-component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ContactMain } from './contact-main/contact-main';
 import { ContactForm } from './contact-form/contact-form';
 import { Footer } from './footer/footer';
+import { PersonalLogo } from '../hero/header/personal-logo/personal-logo';
 
 @Component({
   selector: 'app-contact-component',
-  imports: [ContactMain, ContactForm, Footer],
+  imports: [ContactMain, ContactForm, Footer, PersonalLogo],
   templateUrl: './contact-component.html',
   styleUrl: './contact-component.scss',
 })
--- src/app/sections/peer-reviews/review-title/review-title.ts
+++ src/app/sections/peer-reviews/review-title/review-title.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-review-title',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './review-title.html',
   styleUrl: './review-title.scss',
 })
-export class ReviewTitle {
-
-}
+export class ReviewTitle {}
