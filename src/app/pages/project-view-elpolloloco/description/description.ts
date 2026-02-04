0daa607bb08cd2e52a11a7f20086a7ea77d6bf0b
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-04 10:11:42 +0100

    translation of projectpages

--- src/app/pages/project-view-elpolloloco/description/description.ts
+++ src/app/pages/project-view-elpolloloco/description/description.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-description',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './description.html',
   styleUrl: './description.scss',
 })
-export class Description {
-
-}
+export class Description {}
