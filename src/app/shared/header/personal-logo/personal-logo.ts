2b3a0ff36825687ca9a286c72e1d2913c0dec430
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-04 14:15:46 +0100

    background invert functionality on header elements

--- src/app/shared/header/personal-logo/personal-logo.ts
+++ src/app/shared/header/personal-logo/personal-logo.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-personal-logo',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './personal-logo.html',
   styleUrl: './personal-logo.scss',
 })
