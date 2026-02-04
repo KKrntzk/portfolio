52fae75ee2ed083e920af68ca444c7aeb3acc914
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-04 10:44:25 +0100

    arrows added to project pages

--- src/app/pages/project-view-elpolloloco/project-view-elpolloloco.ts
+++ src/app/pages/project-view-elpolloloco/project-view-elpolloloco.ts
@@ -3,10 +3,12 @@ import { Buttons } from './buttons/buttons';
 import { Description } from './description/description';
 import { Title } from './title/title';
 import { Preview } from './preview/preview';
+import { ArrowLeft } from './arrow-left/arrow-left';
+import { ArrowRight } from './arrow-right/arrow-right';
 
 @Component({
   selector: 'app-project-view-elpolloloco',
-  imports: [Buttons, Description, Title, Preview],
+  imports: [Buttons, Description, Title, Preview, ArrowLeft, ArrowRight],
   templateUrl: './project-view-elpolloloco.html',
   styleUrl: './project-view-elpolloloco.scss',
 })
--- src/app/pages/project-view-join/project-view-join.ts
+++ src/app/pages/project-view-join/project-view-join.ts
@@ -3,10 +3,12 @@ import { Buttons } from './buttons/buttons';
 import { Description } from './description/description';
 import { Preview } from './preview/preview';
 import { Title } from './title/title';
+import { ArrowLeft } from './arrow-left/arrow-left';
+import { ArrowRight } from './arrow-right/arrow-right';
 
 @Component({
   selector: 'app-project-view-join',
-  imports: [Buttons, Description, Preview, Title],
+  imports: [Buttons, Description, Preview, Title, ArrowLeft, ArrowRight],
   templateUrl: './project-view-join.html',
   styleUrl: './project-view-join.scss',
 })
