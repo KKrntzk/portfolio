e7da1f4ec2d27057a7414501517ef4c825b1d328
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-05 11:34:03 +0100

    components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { ProjectViewElpolloloco } from './pages/project-view-elpolloloco/project-view-elpolloloco';
 import { Home } from './pages/home/home';
 import { ProjectViewJoin } from './pages/project-view-join/project-view-join';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'project-view-elpolloloco', component: ProjectViewElpolloloco },
   { path: 'project-view-join', component: ProjectViewJoin },
+  { path: 'privacy-policy', component: PrivacyPolicy },
+  { path: 'legal-notice', component: LegalNotice },
 ];
