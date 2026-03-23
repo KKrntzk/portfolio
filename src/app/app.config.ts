21c259f86c56ec87fc33ce377395c5053ddacd56
KKrntzk <sophie.kornetzki@gmail.com>
2026-03-23 09:01:54 +0100

    app config updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, inject } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
@@ -16,6 +16,7 @@ export const appConfig: ApplicationConfig = {
         anchorScrolling: 'enabled',
         scrollPositionRestoration: 'enabled',
       }),
+      withHashLocation()
     ),
     provideHttpClient(),
     provideTranslateService({
@@ -26,6 +27,5 @@ export const appConfig: ApplicationConfig = {
         suffix: '.json',
       }),
     }),
-    provideHttpClient(),
   ],
 };
